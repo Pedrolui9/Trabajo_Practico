4d9c457114dccebf3dda8f0edf83e3fd0a0dd810
Pedrolui9 <pedroinsfran6@gmial.com>
2025-02-23 22:05:39 -0300

    prueba de checkbox ndoikoi

---
+++ Js/abrirVentanaOpciones.js
@@ -0,0 +1,13 @@
+function abrirOpciones() {
+    let ventanaOpciones= document.getElementById("v_Opciones");
+    ventanaOpciones.style.display = "flex";
+    
+    
+}
+
+function cerrarOpciones() {
+    let ventanaOpciones= document.getElementById("v_Opciones");
+    ventanaOpciones.style.display = "none";
+    
+    
+}
--- Js/sonido_Menu.js
+++ Js/sonido_Menu.js
@@ -1,14 +1,28 @@
- const audio = document.getElementById("audio");
-        const button = document.getElementById("toggleButton");
+// const audio = document.getElementById("audio");
+// const button = document.getElementById("toggleButton");
 
-        function toggleSound() {
-            if (audio.paused) {
-                audio.play();
-                button.textContent = "🔇 Apagar sonido";
-            } else {
-                audio.pause();
-                button.textContent = "🔊 Encender sonido";
-            }
-        }
+// function toggleSound() {
+//     if (audio.paused) {
+//          audio.play();
+//          button.textContent = "🔇 Apagar sonido";
+//      } 
+//     else {
+//         audio.pause();
+//          button.textContent = "🔊 Encender sonido";
+//      }
+     
+// }
 
-        button.addEventListener("click", toggleSound);
+//  button.addEventListener("click", toggleSound);
+
+const musicaDeMenu =  document.getElementById("musicaFondo");
+const musicamp4 = document.getElementById("audio");
+
+musicaDeMenu.addEventListener( function() {
+    if (this.checked) {
+        musicaDeMenu.play();}
+    else {
+        musicaDeMenu.pause();
+        musicaDeMenu.curretTime = 0;
+    }
+})
